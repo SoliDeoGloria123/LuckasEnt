950c9a88219ab05f9412c5cca0b7b1002ef65881
SoliDeoGloria123 <juan.juand.dev@gmail.com>
2025-04-09 16:59:47 -0500

    The scraping of the D1 page of the fruits and vegetables category was performed.

--- trim_2/3.elb_nav/SRC/back_end/web_scrapping/index.py
+++ trim_2/3.elb_nav/SRC/back_end/web_scrapping/index.py
@@ -48,13 +48,13 @@ def extraer_info(URL: str) -> IOResult[str, extraer_infoError]:
         print(f"Launching Playwright for URL: {URL}")  # Mensaje de depuración
         with sync_playwright() as p:
             browser = p.chromium.launch(
-                headless=True
+                headless=False
             )  # Asegúrate de que headless=False
             print("Browser launched successfully.")  # Mensaje de depuración
             page = browser.new_page()
-            page.goto(URL, timeout=60000)
+            page.goto(URL)
             print(f"Navigated to URL: {URL}")  # Mensaje de depuración
-            page.wait_for_load_state("networkidle")
+            #page.wait_for_load_state("networkidle")
             content = page.content()
             print("Page content retrieved.")  # Mensaje de depuración
             browser.close()
@@ -218,135 +218,31 @@ def extract_object_PLP(
         return extract_object_PLPError(Error)
     
         
-def extract_object_PLP_general(card: Tag, partner: str = "") -> IOResult[dict, extract_object_PLPError]:
-     try:
-        if partner == "Jumbo":
-            product_name_raw = (
-                card.select_one('h3[class*="vtex-product-summary-2-x-productNameContainer"]').text.strip()
-                if card.select_one('h3[class*="vtex-product-summary-2-x-productNameContainer"]')
+def extract_object_PLP_general(card: Tag, parner: str = "") -> IOResult[dict, extract_object_PLPError]:
+    try:
+        return {
+            "Parner": parner,
+            "Imagen": extract_image_src(card),
+            "Product Name": (
+                card.select_one('p[class*="prod__name"]').text.strip()
+                if card.select_one('p[class*="prod__name"]')
                 else NOT_FOUND
-            )
-
-            # Limpiar nombre quitando precios y cortar última palabra si es peso
-            product_name = (
-                re.sub(r"\b\d+(\.\d+)?\s?(pesos|PESOS|COP|usd|USD)?\b", "", product_name_raw)
-                .strip()
-                .rsplit(" ", 1)[0]
-                if re.search(r"(\d+\s?(?:kg|g|ml|l|un|unidad|gr))$", product_name_raw, re.IGNORECASE)
-                else re.sub(r"\b\d+(\.\d+)?\s?(pesos|PESOS|COP|usd|USD)?\b", "", product_name_raw).strip()
-            )
-
-            # Buscar peso
-            peso = (
-                re.search(r"(\d+\s?(?:kg|g|ml|l|un|unidad|gr))$", product_name_raw, re.IGNORECASE)
-                .group(1)
-                .upper()
-                if re.search(r"(\d+\s?(?:kg|g|ml|l|un|unidad|gr))$", product_name_raw, re.IGNORECASE)
+            ),
+            "Total_Price": (
+                card.select_one('p[class*="base__price"]').text.strip()
+                if card.select_one('p[class*="base__price"]')
                 else NOT_FOUND
-            )
-
-            return IOSuccess({
-                "Partner": partner,
-                "Imagen": extract_image_src(card),
-                "Product Name": product_name,
-                "Weight": peso,
-                "Total Price": adjust_prices(
-                    (
-                        card.select_one('div[class*="pr2 items-stretch"]').text.strip()
-                        if card.select_one('div[class*="pr2 items-stretch"]')
-                        else NOT_FOUND
-                    ),
-                    (
-                        card.select_one('div[class*="pr2 items-stretch"]').text.strip()
-                        if card.select_one('div[class*="pr2 items-stretch"]')
-                        else NOT_FOUND
-                    )
-                )[0],
-                "Precio con Descuento": adjust_prices(
-                    (
-                        card.select_one('spadivn[class*="flex items-center justify-center tiendasjumboqaio-jumbo-minicart-2-x-containerFlag "]').text.strip()
-                        if card.select_one('div[class*="flex items-center justify-center tiendasjumboqaio-jumbo-minicart-2-x-containerFlag"]')
-                        else NOT_FOUND
-                    ),
-                    (
-                        card.select_one('div[class*="flex items-center justify-center tiendasjumboqaio-jumbo-minicart-2-x-containerFlag"]').text.strip()
-                        if card.select_one('div[class*="flex items-center justify-center tiendasjumboqaio-jumbo-minicart-2-x-containerFlag"]')
-                        else NOT_FOUND
-                    )
-                )[1],
-            })
-
-        else:
-            return IOFailure(extract_object_PLPError(Exception(f"Partner '{partner}' no implementado aún.")))
-     except Exception as Error:
-         return extract_object_PLPError(Error)
-         
-         
-def extract_object_PLP_general(card: Tag, partner: str = "") -> IOResult[dict, extract_object_PLPError]:
-     try:
-        if partner == "Jumbo":
-            product_name_raw = (
-                card.select_one('h3[class*="vtex-product-summary-2-x-productNameContainer mv0"]').text.strip()
-                if card.select_one('h3[class*="vtex-product-summary-2-x-productNameContainer mv0"]')
+            ),
+            "Precio_con_Descuento": (
+                card.select_one('p[class*="base__price"]').text.strip()
+                if card.select_one('p[class*="base__price"]')
                 else NOT_FOUND
-            )
+            ),
 
-            # Limpiar nombre quitando precios y cortar última palabra si es peso
-            product_name = (
-                re.sub(r"\b\d+(\.\d+)?\s?(pesos|PESOS|COP|usd|USD)?\b", "", product_name_raw)
-                .strip()
-                .rsplit(" ", 1)[0]
-                if re.search(r"(\d+\s?(?:kg|g|ml|l|un|unidad|gr))$", product_name_raw, re.IGNORECASE)
-                else re.sub(r"\b\d+(\.\d+)?\s?(pesos|PESOS|COP|usd|USD)?\b", "", product_name_raw).strip()
-            )
-
-            # Buscar peso
-            peso = (
-                re.search(r"(\d+\s?(?:kg|g|ml|l|un|unidad|gr))$", product_name_raw, re.IGNORECASE)
-                .group(1)
-                .upper()
-                if re.search(r"(\d+\s?(?:kg|g|ml|l|un|unidad|gr))$", product_name_raw, re.IGNORECASE)
-                else NOT_FOUND
-            )
-
-            return IOSuccess({
-                "Partner": partner,
-                "Imagen": extract_image_src(card),
-                "Product Name": product_name,
-                "Weight": peso,
-                "Total Price": adjust_prices(
-                    (
-                        card.select_one('div[class*="tiendasjumboqaio-jumbo-minicart-2-x-price"]').text.strip()
-                        if card.select_one('div[class*="tiendasjumboqaio-jumbo-minicart-2-x-price"]')
-                        else NOT_FOUND
-                    ),
-                    (
-                        card.select_one('div[class*="tiendasjumboqaio-jumbo-minicart-2-x-price"]').text.strip()
-                        if card.select_one('div[class*="tiendasjumboqaio-jumbo-minicart-2-x-price"]')
-                        else NOT_FOUND
-                    )
-                )[0],
-                "Precio con Descuento": adjust_prices(
-                    (
-                        card.select_one('spadivn[class*="flex items-center justify-center tiendasjumboqaio-jumbo-minicart-2-x-containerFlag "]').text.strip()
-                        if card.select_one('div[class*="flex items-center justify-center tiendasjumboqaio-jumbo-minicart-2-x-containerFlag"]')
-                        else NOT_FOUND
-                    ),
-                    (
-                        card.select_one('div[class*="flex items-center justify-center tiendasjumboqaio-jumbo-minicart-2-x-containerFlag"]').text.strip()
-                        if card.select_one('div[class*="flex items-center justify-center tiendasjumboqaio-jumbo-minicart-2-x-containerFlag"]')
-                        else NOT_FOUND
-                    )
-                )[1],
-            })
-
-        else:
-            return IOFailure(extract_object_PLPError(Exception(f"Partner '{partner}' no implementado aún.")))
-     except Exception as Error:
-         return extract_object_PLPError(Error) 
-
-
-
+        }
+    except Exception as Error:
+        return IOFailure(extract_object_PLPError(Error))
+ 
 
 # Extrae el atributo 'src' de una etiqueta <img> directamente con funciones encadenadas
 def extract_image_src(card: Tag) -> str:
